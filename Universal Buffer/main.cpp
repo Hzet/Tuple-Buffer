b91b8f601f9723e11e89a4352e3eea66ac3471f5
Hzet <jedjas7@gmail.com>
2020-12-02 19:47:30 +0100

    Developed simple buffer - ran a very simple test

---
+++ Universal Buffer/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "universal-buffer.hpp"
+
+#define PRINT(x) std::cout << #x << " = " << x << std::endl;
+
+template <std::size_t I = 0u, class... Args>
+void print(const haz::CTupleBuffer<Args...> &ub, std::size_t index)
+{
+	std::cout << ub.get<I>(index);
+
+	if constexpr (I + 1u < sizeof...(Args))
+	{
+		std::cout << ", ";
+		print<I + 1u>(ub, index);
+	}
+	else
+		std::cout << std::endl;
+}
+
+template <class... Args>
+void print(const haz::CTupleBuffer<Args...> &ub)
+{
+	const std::size_t size = ub.getCount();
+	for (std::size_t i = 0u; i < size; i++)
+		print(ub, i);
+}
+int main()
+{
+	haz::CTupleBuffer<std::uint32_t, std::uint64_t, std::uint8_t> u32_64_8;
+	haz::CTupleBuffer<std::uint16_t, std::uint16_t, std::uint8_t> u16_16_8;
+	haz::CTupleBuffer<std::uint64_t, std::uint64_t, std::uint64_t> u64_64_64;
+
+	PRINT(u32_64_8.getCount());
+	PRINT(u16_16_8.getCount());
+	PRINT(u64_64_64.getCount());
+	PRINT(u32_64_8.getStrideSize());
+	PRINT(u16_16_8.getStrideSize());
+	PRINT(u64_64_64.getStrideSize());
+
+	u32_64_8.push_back(0xFFFFFFFF, 0xFFFF, 0xF);
+	u16_16_8.push_back(0xFFFFFF, 0xFF, 0xF);
+	u64_64_64.push_back(0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF);
+
+	print(u32_64_8);
+	print(u16_16_8);
+	print(u64_64_64);
+}
---
+++ Universal Buffer/universal-buffer.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <vector>
+
+#include "universal-buffer-base.hpp"
+
+namespace haz
+{
+	template <class... Args>
+	class CTupleBuffer
+		: private impl::CTupleBufferBase<Args...>
+	{
+	public:
+		CTupleBuffer() noexcept;
+		explicit CTupleBuffer(Args&&... elems);
+		explicit CTupleBuffer(const Args&... elems);
+
+		~CTupleBuffer();
+		CTupleBuffer(CTupleBuffer&&) = default;
+		CTupleBuffer(const CTupleBuffer&) = default;
+
+		CTupleBuffer& operator=(CTupleBuffer&&) = default;
+		CTupleBuffer& operator=(const CTupleBuffer&) = default;
+
+		void push_back(Args&&... elems);
+		void push_back(const Args&... elems);
+
+		void clear();
+		void resize(std::size_t count);
+
+		std::size_t getSize() const;		// size in bytes
+		std::size_t getCount() const;		// elements
+		std::size_t getStrideSize() const;  // parameter size in bytes
+		std::size_t getStrideCount() const; // parameter count
+
+		template <std::size_t I> auto& get(std::size_t index);
+		template <std::size_t I> const auto& get(std::size_t index) const;
+
+	private:
+		template <std::size_t I> auto * const getPointer(std::size_t index);
+		template <std::size_t I> auto const * const getPointer(std::size_t index) const;
+
+		template <std::size_t I> std::byte * const getStoragePointer(std::size_t index);
+		template <std::size_t I> std::byte const * const getStoragePointer(std::size_t index) const;
+
+		std::vector<std::byte> data_;
+	};
+
+#include "universal-buffer.inl"
+}	
